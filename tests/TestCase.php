a4f42b438a3b47fce94a3013154721bdb4287b54
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2017-02-27 13:05:35 +0100

    initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Absolvent\swagger\tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+}
