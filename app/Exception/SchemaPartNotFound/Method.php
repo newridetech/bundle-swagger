bb4b6080a3d97afe155b0b98ceb06541a61b9d1d
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2017-03-07 13:46:35 +0100

    pass schema objects to SchemaPartNotFound exceptions

--- app/Exception/SchemaPartNotFound/Method.php
+++ app/Exception/SchemaPartNotFound/Method.php
@@ -2,8 +2,18 @@
 
 namespace Absolvent\swagger\Exception\SchemaPartNotFound;
 
+use Absolvent\swagger\Breadcrumbs;
 use Absolvent\swagger\Exception\SchemaPartNotFound;
+use stdClass;
 
 class Method extends SchemaPartNotFound
 {
+    public $requestPathSchema;
+
+    public function __construct(Breadcrumbs $breadcrumbs, stdClass $requestPathSchema)
+    {
+        parent::__construct($breadcrumbs);
+
+        $this->requestPathSchema = $requestPathSchema;
+    }
 }
--- app/Exception/SchemaPartNotFound/StatusCode.php
+++ app/Exception/SchemaPartNotFound/StatusCode.php
@@ -2,8 +2,18 @@
 
 namespace Absolvent\swagger\Exception\SchemaPartNotFound;
 
+use Absolvent\swagger\Breadcrumbs;
 use Absolvent\swagger\Exception\SchemaPartNotFound;
+use stdClass;
 
 class StatusCode extends SchemaPartNotFound
 {
+    public $requestMethodSchema;
+
+    public function __construct(Breadcrumbs $breadcrumbs, stdClass $requestMethodSchema)
+    {
+        parent::__construct($breadcrumbs);
+
+        $this->requestMethodSchema = $requestMethodSchema;
+    }
 }
