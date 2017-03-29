29e756195f4399ee13fd77096437fe09541c9c0b
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2017-03-29 21:32:58 +0200

    remove unnecessary mapping

--- app/JsonSchema/RequestParameters.php
+++ app/JsonSchema/RequestParameters.php
@@ -12,10 +12,6 @@ class RequestParameters extends JsonSchema implements IteratorAggregate
 {
     public function getIterator(): Iterator
     {
-        $data = array_map(function (stdClass $item) {
-            return json_decode(json_encode($item));
-        }, $this->data);
-
-        return new ArrayIterator($data);
+        return new ArrayIterator($this->data);
     }
 }
