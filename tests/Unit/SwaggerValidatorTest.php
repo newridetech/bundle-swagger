9094bd75e789713c39e1d46a01da730daeee620e
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2017-04-03 16:56:49 +0200

    uncomment SwaggerValidator tests

--- tests/Unit/SwaggerValidatorTest.php
+++ tests/Unit/SwaggerValidatorTest.php
@@ -11,67 +11,67 @@ class SwaggerValidatorTest extends TestCase
 {
     public function provideRequestResponse()
     {
-        // yield 'valid request-response' => [
-        //     'fixtureFilename' => 'fixtures/petstore.yml',
-        //     'request' => Request::create('http://example.com/api/pet', 'GET'),
-        //     'isRequestValid' => true,
-        //     'response' => Response::create(json_encode([
-        //         [
-        //             'pet_id' => 1,
-        //             'pet_name' => 'test',
-        //         ],
-        //     ]), 200),
-        //     'isResponseValid' => true,
-        // ];
-        //
-        // yield 'extra field' => [
-        //     'fixtureFilename' => 'fixtures/petstore.yml',
-        //     'request' => Request::create('http://example.com/api/pet', 'GET'),
-        //     'isRequestValid' => true,
-        //     'response' => Response::create(json_encode([
-        //         [
-        //             'pet_id' => 2,
-        //             'pet_namez' => 'bar',
-        //         ],
-        //     ]), 200),
-        //     'isResponseValid' => false,
-        // ];
-        //
-        // yield 'type coercion' => [
-        //     'fixtureFilename' => 'fixtures/petstore.yml',
-        //     'request' => Request::create('http://example.com/api/pet', 'GET'),
-        //     'isRequestValid' => true,
-        //     'response' => Response::create(json_encode([
-        //         [
-        //             'pet_id' => '1',
-        //             'pet_name' => 'test',
-        //         ],
-        //         [
-        //             'pet_id' => '2',
-        //             'pet_name' => 'bar',
-        //         ],
-        //     ]), 200),
-        //     'isResponseValid' => true,
-        // ];
-        //
-        // yield 'at least one item' => [
-        //     'fixtureFilename' => 'fixtures/petstore.yml',
-        //     'request' => Request::create('http://example.com/api/pet', 'GET'),
-        //     'isRequestValid' => true,
-        //     'response' => Response::create(json_encode([]), 200),
-        //     'isResponseValid' => false,
-        // ];
-        //
-        // yield 'valid input parameters' => [
-        //     'fixtureFilename' => 'fixtures/petstore-expanded.yml',
-        //     'request' => Request::create('http://petstore.swagger.io/api/pets', 'GET', [
-        //         'tags' => 0,
-        //         'limit' => 0,
-        //     ]),
-        //     'isRequestValid' => true,
-        //     'response' => Response::create(json_encode([]), 200),
-        //     'isResponseValid' => true,
-        // ];
+        yield 'valid request-response' => [
+            'fixtureFilename' => 'fixtures/petstore.yml',
+            'request' => Request::create('http://example.com/api/pet', 'GET'),
+            'isRequestValid' => true,
+            'response' => Response::create(json_encode([
+                [
+                    'pet_id' => 1,
+                    'pet_name' => 'test',
+                ],
+            ]), 200),
+            'isResponseValid' => true,
+        ];
+
+        yield 'extra field' => [
+            'fixtureFilename' => 'fixtures/petstore.yml',
+            'request' => Request::create('http://example.com/api/pet', 'GET'),
+            'isRequestValid' => true,
+            'response' => Response::create(json_encode([
+                [
+                    'pet_id' => 2,
+                    'pet_namez' => 'bar',
+                ],
+            ]), 200),
+            'isResponseValid' => false,
+        ];
+
+        yield 'type coercion' => [
+            'fixtureFilename' => 'fixtures/petstore.yml',
+            'request' => Request::create('http://example.com/api/pet', 'GET'),
+            'isRequestValid' => true,
+            'response' => Response::create(json_encode([
+                [
+                    'pet_id' => '1',
+                    'pet_name' => 'test',
+                ],
+                [
+                    'pet_id' => '2',
+                    'pet_name' => 'bar',
+                ],
+            ]), 200),
+            'isResponseValid' => true,
+        ];
+
+        yield 'at least one item' => [
+            'fixtureFilename' => 'fixtures/petstore.yml',
+            'request' => Request::create('http://example.com/api/pet', 'GET'),
+            'isRequestValid' => true,
+            'response' => Response::create(json_encode([]), 200),
+            'isResponseValid' => false,
+        ];
+
+        yield 'valid input parameters' => [
+            'fixtureFilename' => 'fixtures/petstore-expanded.yml',
+            'request' => Request::create('http://petstore.swagger.io/api/pets', 'GET', [
+                'tags' => 0,
+                'limit' => 0,
+            ]),
+            'isRequestValid' => true,
+            'response' => Response::create(json_encode([]), 200),
+            'isResponseValid' => true,
+        ];
 
         yield 'invalid input parameters' => [
             'fixtureFilename' => 'fixtures/petstore-expanded.yml',
